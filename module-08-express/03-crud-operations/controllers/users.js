d96b241cfd4a9ac284f01c062a838ee73a486f16
anoj-kt <anojk28@gmail.com>
2025-07-22 17:20:39 +0200

    Added complete crud operation exercise solution

--- module-08-express/03-crud-operations/controllers/users.js
+++ module-08-express/03-crud-operations/controllers/users.js
@@ -1,13 +1,25 @@
 import User from '../models/User.js';
+import Note from '../models/Note.js';
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll({ include: Note });
+
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const createUser = async (req, res) => {
   try {
-    const { firstName, lastName, email } = req.body;
+    const { firstName, lastName } = req.body;
 
     if (!firstName || !lastName)
       return res.json({ message: 'First name and last name is required' });
 
     const newUser = await User.create(req.body);
+
     res.json(newUser);
   } catch (error) {
     console.log(error);
@@ -15,4 +27,53 @@ const createUser = async (req, res) => {
   }
 };
 
-export { createUser };
+const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findByPk(id, { include: Note });
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const {
+      body: { firstName, lastName, email },
+      params: { id },
+    } = req;
+
+    if (!firstName || !lastName || !email)
+      return res
+        .status(400)
+        .json({ error: 'firstName, lastName, and email are required' });
+
+    const user = await User.findByPk(id);
+    if (!user) return res.status(404).json({ error: 'User not found' });
+    await user.update(req.body);
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findByPk(id);
+    if (!user) return res.status(404).json({ error: 'User not found' });
+    await user.destroy();
+
+    res.json({ message: 'User deleted' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export { getAllUsers, createUser, getUserById, updateUser, deleteUser };
--- module-08-express/03-crud-operations/index.js
+++ module-08-express/03-crud-operations/index.js
@@ -1,21 +1,40 @@
 import express from 'express';
-import { createUser } from './controllers/users.js';
-import { createNote, getAllNotes } from './controllers/notes.js';
+import {
+  createUser,
+  getAllUsers,
+  updateUser,
+  deleteUser,
+} from './controllers/users.js';
+import {
+  createNote,
+  getAllNotes,
+  getNoteById,
+  updateNote,
+  deleteNote,
+} from './controllers/notes.js';
 
 const app = express();
 const port = 8080;
 
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
+
+// The below endpoint was just to test. We can delete it
+// app.get('/', (req, res) => {
+//   res.send('Hello world!');
+// });
 
 //User Routes
 app.post('/user', createUser);
+app.get('/users', getAllUsers);
+app.put('/user/:id', updateUser);
+app.delete('/user/:id', deleteUser);
 
 //Notes Routes
 app.post('/notes', createNote);
 app.get('/notes', getAllNotes);
+app.get('/notes/:id', getNoteById);
+app.put('/notes/:id', updateNote);
+app.delete('/notes/:id', deleteNote);
 
 app.listen(port, () =>
   console.log(`Server is running on http://localhost:${port}`)
