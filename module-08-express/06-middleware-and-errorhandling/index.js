58b56e3347a8bcb5a957ad6335c0d719558f8d89
anoj-kt <anojk28@gmail.com>
2025-07-24 14:59:32 +0200

    Added error handling lecture code

--- module-08-express/06-middleware-and-errorhandling/index.js
+++ module-08-express/06-middleware-and-errorhandling/index.js
@@ -3,6 +3,7 @@ import './db/associations.js';
 import userRouter from './routes/userRoutes.js';
 import postRouter from './routes/postRoutes.js';
 import middleware from './middlewares/middleware.js';
+import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -11,5 +12,10 @@ app.use(express.json()); //Application level middleware
 // app.use(middleware); //Application level middleware
 app.use('/users', userRouter);
 app.use('/posts', postRouter);
+
+app.use((req, res) => {
+  throw new Error('Page not found');
+});
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
