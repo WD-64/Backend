1e2dc2488c3af82953cbc2f00e0260453a6a8688
RobinBigus <robin.bigus@wbscodingschool.com>
2025-08-13 10:44:49 +0200

    Added streaming frontend code and readme file

--- module-11-AI/chatbot-gemini/src/App.jsx
+++ module-11-AI/chatbot-gemini/src/App.jsx
@@ -1,42 +1,47 @@
-import { useState, useRef, useEffect } from 'react';
-import { ToastContainer } from 'react-toastify';
-import './index.css';
-import Form from './components/Form';
-import Chat from './components/Chat';
-import { getChatHistory } from './data/gemini';
+import { useState, useRef, useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+import "./index.css";
+import Form from "./components/Form";
+import Chat from "./components/Chat";
+import { getChatHistory } from "./data/gemini";
 
 function App() {
   // let us reference DOM element for scroll effect
   const chatRef = useRef();
   const [messages, setMessages] = useState([]);
-  const [chatId, setChatId] = useState(localStorage.getItem('chatId'));
+  const [chatId, setChatId] = useState(localStorage.getItem("chatId"));
   console.log(messages);
 
   // scroll to bottom of chat when new message is added
   useEffect(() => {
     chatRef.current?.lastElementChild?.scrollIntoView({
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   }, [messages]);
 
   useEffect(() => {
-    const setChatHistory = async () => {
+    const getAndSetChatHistory = async () => {
       try {
-        const response = await getChatHistory(chatId);
-        setMessages(response.history);
+        const { history } = await getChatHistory(chatId);
+        setMessages(history);
       } catch (error) {
-        console.log(error);
+        localStorage.removeItem("chatId");
       }
     };
 
-    chatId && setChatHistory();
+    chatId && getAndSetChatHistory();
   }, []);
 
   return (
-    <div className='h-screen container mx-auto p-5 flex flex-col justify-between gap-5'>
+    <div className="h-screen container mx-auto p-5 flex flex-col justify-between gap-5">
       <Chat chatRef={chatRef} messages={messages} />
-      <Form chatRef={chatRef} setMessages={setMessages} chatId={chatId} />
-      <ToastContainer autoClose={1500} theme='colored' />
+      <Form
+        chatRef={chatRef}
+        setMessages={setMessages}
+        chatId={chatId}
+        setChatId={setChatId}
+      />
+      <ToastContainer autoClose={1500} theme="colored" />
     </div>
   );
 }
--- module-11-AI/chatbot-gemini/src/data/gemini.js
+++ module-11-AI/chatbot-gemini/src/data/gemini.js
@@ -1,18 +1,35 @@
 const API_URL = import.meta.env.VITE_API_URL;
 if (!API_URL)
-  throw new Error('API URL is required, are you missing a .env file?');
+  throw new Error("API URL is required, are you missing a .env file?");
 const baseURL = `${API_URL}/chat`;
+
+const fetchChat = async (body) => {
+  const response = await fetch(baseURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  if (!response.ok) {
+    // If the response is not ok, throw an error by parsing the JSON response
+    const { error } = await response.json();
+    throw new Error(error || "Something went wrong");
+  }
+
+  return response;
+};
 
 const createChat = async (body) => {
   const response = await fetch(baseURL, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(body),
   });
 
-  if (!response.ok) throw new Error('AI failed to respond');
+  if (!response.ok) throw new Error("AI failed to respond");
 
   const data = await response.json();
 
@@ -22,11 +39,11 @@ const createChat = async (body) => {
 const getChatHistory = async (chatId) => {
   const response = await fetch(`${baseURL}/${chatId}`);
 
-  if (!response.ok) throw new Error('Failed to fetch chat history');
+  if (!response.ok) throw new Error("Failed to fetch chat history");
 
   const data = await response.json();
 
   return data;
 };
 
-export { createChat, getChatHistory };
+export { createChat, getChatHistory, fetchChat };
